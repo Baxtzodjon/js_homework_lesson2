e985bb936f4a6d60d443bb72e52311c3af04557a
Rasulov Baxtzod <141494256+Baxtzodjon@users.noreply.github.com>
2023-08-11 15:16:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let str = 'aaa@bbb@ccc aaa!bbb!ccc'
+console.log(`str is ${str}`);
+
+let srt = 'aaa@bbb@ccc'
+console.log(srt);
+
+srt = 'aaa!bbb!ccc'
+console.log(srt);
+
+
+
+let string = 'HELLOWORLD Hello world'
+console.log(`string is ${string}`);
+
+let hello = 'HELLOWORLD'
+console.log(hello);
+
+srt = 'Hello world'
+console.log(srt);
+
+
+
+let text = `Hello, it is HTML Hello it is not JS`
+console.log(`text is ${text}`);
+
+let text2 = 'Hello, it is HTML'
+console.log(text2);
+
+hello_text = 'Hello it is not JS'
+console.log(hello_text);
+
+
+
+`let a = 'alex'`
+
+let a = 'alex'
+console.log(a);
+
+
+
+let math = Math.random()
+console.log(math);
+
+
+
+let Alex  = 'Lex Luter has a big suit'
+console.log(Alex);
+
+Alex = 'Alex'
+console.log(Alex);
